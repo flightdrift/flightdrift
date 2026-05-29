3e44c835c35cc41453255bbad79a1e5e9ed55042
Jamius Siam <j@sii.am>
2026-05-29 21:15:09 +0600

    Removed unused imports and updated UI with logo

--- flightdrift-frontend/src/routes/index.tsx
+++ flightdrift-frontend/src/routes/index.tsx
@@ -2,18 +2,14 @@
  * Author: Jamius Siam
  * Since: 06/05/2026
  */
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import type { JSX } from "react";
-import { Button } from "@/components/ui/button.tsx";
 
 // TODO: Implement a default login page later
 const Index = (): JSX.Element => {
   return (
     <div className="flex flex-col gap-3 items-center justify-center w-full h-screen">
-      Flightdrift Login
-      <Button>
-        <Link to={"/dash/item"}>Dashboard</Link>
-      </Button>
+      <img src="/flightdrift_logo_light.svg" alt="Flightdrift Logo" className="h-7" />
     </div>
   );
 };
